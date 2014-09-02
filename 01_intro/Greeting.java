ce08ff7b60a5fb604487fe7eb740ca0837d19b70
Claus Bové <clbo@kea.dk>
2014-09-02 15:10:49 +0200

    files added

---
+++ 01_intro/Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting
+{
+   public static void main(String[] args)
+   {
+      // Dette er er en kommentar
+      System.out.println("Hello World 34");
+   }
+}
---
+++ 01_intro/Tent.java
@@ -0,0 +1,10 @@
+public class Tent
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hej Verden");
+      System.out.println("Hello there");
+      System.out.print("Halli hallodig der");
+      System.out.print("dig der");
+   }
+}
