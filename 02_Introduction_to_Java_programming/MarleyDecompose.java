dc97058ecbc5d1c2da62fe0fafc245803c0d3f4a
Claus Bové <clbo@kea.dk>
2014-09-04 10:20:02 +0200

    marley added

---
+++ 02_Introduction_to_Java_programming/MarleyDecompose.java
@@ -0,0 +1,59 @@
+public class MarleyDecompose
+{
+	public static void main(String[] args)
+	{
+		/* Print out this text in the console
+      
+      Get up, stand up: stand up for your rights!
+		Get up, stand up: stand up for your rights!
+		Get up, stand up: stand up for your rights!
+		Get up, stand up: don't give up the fight! 
+      
+      Preacher man, don't tell me,
+      Heaven is under the earth.
+      I know you don't know
+      What life is really worth.
+      It's not all that glitters is gold;
+      'Alf the story has never been told:
+      So now you see the light, eh!
+      Stand up for your rights. come on!
+      
+		Get up, stand up: stand up for your rights!
+		Get up, stand up: don't give up the fight!
+		Get up, stand up: stand up for your rights!
+		Get up, stand up: don't give up the fight! 
+      
+      */
+
+		// Step 1: print text in single lines
+		System.out.println("Get up, stand up: stand up for your rights!");
+		System.out.println("Get up, stand up: stand up for your rights!");		
+      System.out.println("Get up, stand up: stand up for your rights!");
+		System.out.println("Get up, stand up: don't give up the fight!");
+      
+      System.out.println();
+
+      System.out.println("Preacher man, don't tell me,");
+      System.out.println("Heaven is under the earth.");
+      System.out.println("I know you don't know");
+      System.out.println("What life is really worth.");
+      System.out.println("It's not all that glitters is gold;");
+      System.out.println("'Alf the story has never been told:");
+      System.out.println("So now you see the light, eh!");
+      System.out.println("Stand up for your rights. come on!");
+      
+      System.out.println();
+
+      System.out.println("Get up, stand up: stand up for your rights!");
+		System.out.println("Get up, stand up: don't give up the fight!");		
+      System.out.println("Get up, stand up: stand up for your rights!");
+		System.out.println("Get up, stand up: don't give up the fight!");
+      
+      /* 
+         Change the program so it uses methods where posible.
+         This will make the program easyer to read by showing 
+         structure and it will reduce or eliminate redundancy. 
+      */ 
+     
+	}
+}
