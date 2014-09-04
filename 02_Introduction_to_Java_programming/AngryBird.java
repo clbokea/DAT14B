48323c0d652dac094d3759157702bc1593fb1553
Claus Bové <clbo@kea.dk>
2014-09-04 10:18:29 +0200

    files added

---
+++ 02_Introduction_to_Java_programming/AngryBird.java
@@ -0,0 +1,24 @@
+public class AngryBird
+{
+   public static void main(String[] args)
+   { 
+      // Metode Kald
+       
+       moveForward();
+       moveForward();
+       moveForward();
+       turnRight();  
+   }
+   
+   // Declare Method
+   public static void moveForward()
+   {
+      System.out.println("Step!");
+   }
+   
+   public static void turnRight()
+   {
+      System.out.println("wriiii!");
+   }
+      
+}
---
+++ 02_Introduction_to_Java_programming/BakeCake.java
@@ -0,0 +1,28 @@
+public class BakeCake
+{
+   public static void main(String[] args)
+   {
+      //Metodekald
+      makeDough();
+      bake();
+      makeFrosting();
+      decorate();
+   }
+   
+   public static void makeDough()
+   {
+      System.out.println("Bland: mel, salt gær");
+      System.out.println("pisj æg");
+      System.out.println("Bland alt");
+   }
+   public static void bake()
+   {
+   }
+   public static void makeFrosting()
+   {
+   }
+   public static void decorate()
+   {
+   }
+   
+}
