3b0a7b742f7f4fa071dbb5722866abf0c60588dd
Claus Bové <clbo@kea.dk>
2014-09-04 22:00:40 +0200

    marley solution

---
+++ 02_Introduction_to_Java_programming/MarleyDecomposeSolution.java
@@ -0,0 +1,50 @@
+public class MarleyDecomposeSolution
+{
+	public static void main(String[] args)
+	{
+   
+      chorus1();
+      System.out.println();
+      verse();
+      System.out.println();
+      chorus2(); 
+     
+	}
+   
+   public static void chorus1()
+   {
+     right();
+     right();
+     right();
+    fight(); fight();
+   }
+   public static void verse()
+   {
+      System.out.println("Preacher man, don't tell me,");
+      System.out.println("Heaven is under the earth.");
+      System.out.println("I know you don't know");
+      System.out.println("What life is really worth.");
+      System.out.println("It's not all that glitters is gold;");
+      System.out.println("'Alf the story has never been told:");
+      System.out.println("So now you see the light, eh!");
+      System.out.println("Stand up for your rights. come on!");
+      
+   }
+   public static void chorus2()
+   {
+      right();
+      fight(); 
+      right();
+      fight();  
+      
+   }
+   public static void fight()
+   {
+      System.out.println("Get up, stand up: don't give up the fight!");
+   }
+   public static void right()
+   {
+      System.out.println("Get up, stand up: stand up for your rights!");
+		
+   }
+}
