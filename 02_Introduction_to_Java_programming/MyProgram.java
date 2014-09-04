0e3ae3cc7cd8ee02a01a0d8033316e58b214251e
Claus Bové <clbo@kea.dk>
2014-09-04 10:19:08 +0200

    file added

---
+++ 02_Introduction_to_Java_programming/MyProgram.java
@@ -0,0 +1,27 @@
+public class MyProgram
+{
+
+public static void main(String[] args)
+{
+   System.out.println("public class MyProgram");
+   System.out.println("{");
+   System.out.println("\tpublic static void main(String[] args)");
+   System.out.println("\t{");
+   System.out.println("\t\tSystem.out.println(\"Hello World\");");
+   System.out.println("\t}");
+   System.out.println("}");
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
