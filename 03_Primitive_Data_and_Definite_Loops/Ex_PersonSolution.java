f0417932f0b08e2906467ab5ae057dce1437dc46
Claus Bové <clbo@kea.dk>
2014-09-09 17:28:16 +0200

    Person solution

---
+++ 03_Primitive_Data_and_Definite_Loops/Ex_PersonSolution.java
@@ -0,0 +1,27 @@
+public class Ex_PersonSolution
+{
+   public static void main(String[] args)
+   {
+      // Make a program that uses varaibles
+      // The program should print out
+      // Name, Age and Height of the person
+       
+      // 1. Declare the variables (! a variable has a name, and a data type)
+      String name = "Claus";
+      int age = 27;
+      double height = 1.80;
+       
+      // 2. Assign values to the variables
+       
+      // 3. print out variables 
+      // Like this: 
+      // My name is XXX
+      // I am XXX years old.
+      // and i am XXX meters tall.
+     System.out.println("My name is " + name);
+     System.out.println("I am " + age + " years old.");
+     System.out.println("and i am " + height +  " meters tall.");
+      
+                   
+   }
+}
