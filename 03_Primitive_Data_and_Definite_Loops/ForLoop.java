ce95445a57df37f362b8d4221d3909a4e1e5ee21
Claus Bové <clbo@kea.dk>
2014-09-09 17:12:07 +0200

    deleted class

---
+++ 03_Primitive_Data_and_Definite_Loops/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop
+{
+   public static void main(String[] args)
+   {
+      // for loop
+ 
+      for(int i = 1; i <= 10; i++)
+      {
+         System.out.println(i * 10);
+         i++;
+      }
+      
+      System.out.println("Mit loop er slut!");
+      
+   }
+}
