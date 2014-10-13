7c278dd0bf43a02975990131acbca65dbc6de24b
Claus Bové <clbo@kea.dk>
2014-10-13 23:42:00 +0200

    11 folder added

---
+++ 11_User_errors/ExerciseBlackJackDiceGame.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ExerciseBlackJackDiceGame 
+{
+   public static void main(String[] args)
+   {
+ 
+      
+      // 1.USER THROWS
+      while () 
+      {
+         
+      }
+      
+      // 2.MACHINE THROWS
+      // This is more or less the same procedure as above
+      // but you do not ask the machine if it wants to go on.
+      // You have to set up some rules (algorithm) that makes 
+      // the machine go on or stop.
+      // And remember that if the user got 21, 
+      // then the machine have already lost.
+      // And if the user exeeded 21, then the machine have already won
+      
+      
+      
+            
+      // 3.CHECK WHO WON
+      // Here you will have to check:
+      // 1. Did the user get 21, then he wins
+      // 2. did the user exeed 21, then machine wins
+      // 3. did the machine exeed 21, then user wins
+      // 4. is the users sum larger than the machines, the user win
+      // 5. is the machine sum larger than users, then machine win
+      // 6. did user and machine get the same sum, then it is a draw
+         
+   }
+
+}
