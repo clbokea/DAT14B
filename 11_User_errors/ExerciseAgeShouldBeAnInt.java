7e01021b946764e714e1c5c7042147a1a27f0306
Claus Bové <clbo@kea.dk>
2014-10-14 00:10:17 +0200

    added

---
+++ 11_User_errors/ExerciseAgeShouldBeAnInt.java
@@ -0,0 +1,25 @@
+public class ExerciseAgeShouldBeAnInt 
+{
+   public static void main(String[] args)
+   {
+            
+      // Write some code that checks if the input is an int
+      // and if not tells the user that it is not an int, 
+      // and asks him again. if it is an int give the user a 
+      // confirmation that it is ok
+      // HINT: when you want to ask the same question again 
+      // and again you could use a loop.
+      
+      
+      // First you need a Scanner Object 
+      
+      // Then you need to ask a question to the user that he can answer
+      
+      // Then you have to check if the input is of type int
+      // If not, tell the user that it is not a valid number or not a number
+      // and ask again
+      // when the input is correct print out the number and an OK message
+      
+   }
+
+  }
