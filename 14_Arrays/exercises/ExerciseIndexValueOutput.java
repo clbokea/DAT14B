3d8f7966343d15ec1a5d78ceb8731ee24a639893
Claus Bové <clbo@kea.dk>
2014-11-04 13:19:28 +0100

    14 added

---
+++ 14_Arrays/exercises/ExerciseIndexValueOutput.java
@@ -0,0 +1,23 @@
+public class ExerciseIndexValueOutput
+{
+   public static void main(String[] args)
+   {  
+      // Crate and array and fill it with equal number values, starting at 2
+      // (2, 4, 6, 8, 10, 12, etc) 
+      // In the console print a table with the header:
+      // "Index" & "Value"
+      // in the next rows print the index number from the array
+      // and the corosponding value  
+      
+      // It should look something like this:
+      
+      //      Index     Value     
+      //      0         2           
+      //      1         4         
+      //      2         6        
+      //      3         8        
+      //      4         10
+      //      ...       ... 
+  
+   }
+}
---
+++ 14_Arrays/exercises/ExerciseSumAnArray.java
@@ -0,0 +1,10 @@
+public class ExerciseSumAnArray
+{
+   public static void main(String[] args)
+   {  
+      // Create an array and fill it with numbers
+      // Create a method that calculates the sum of all numbers in the array
+      // The method takes 1 parameter (the array) and returns an int (the sum)
+   
+   }
+}
